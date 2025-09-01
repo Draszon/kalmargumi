ef7317a564d8ef4f63edb3897f3c3f216b5751a0
Szentgyörgyi Péter <peti0202@windowslive.com>
2025-09-01 07:37:51 +0200

    értékeink rész megvan, galéria elkezdve

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="index-section-wrapper section">
+<section class="index-section-wrapper section">
     <div class="index-content">
         <img src="{{ asset("images/index_section/defekt.jpg") }}" alt="defekt">
         <div>
@@ -13,19 +13,19 @@
         <img src="{{ asset("images/index_section/arrow-left.svg") }}" alt="arrow">
         <img src="{{ asset("images/index_section/arrow-right.svg") }}" alt="arrow">
     </div>
-</div>
+</section>
 
 <div class="divider">
     <img src="{{ asset('images/index_section/divider.svg') }}" alt="szerkció határoló">
 </div>
 
-<div class="values-section-wrapper section">
+<section class="values-section-wrapper section">
     <div class="cards-wrapper">
         <div class="value-card">
             <h2 class="value-title">megbízhatóság</h2>
             <img src="{{ asset("images/values_section/shield.png") }}" alt="" class="value-logo">
         </div>
-        <div class="value-card">
+        <div class="value-card middle">
             <h2 class="value-title">gyorsaság</h2>
             <img src="{{ asset("images/values_section/lightning.png") }}" alt="" class="value-logo">
         </div>
@@ -35,7 +35,20 @@
         </div>
     </div>
     <p class="values-text">
-        Cégünk többéves tapasztalattal áll az Ön rendelkezésére, hogy autója mindig biztonságosan és zökkenőmentesen közlekedhessen. A legmodernebb technológiával és szakértő csapattal végezzük el a gumiabroncsok cseréjét, javítását, valamint a futómű-beállítást.
+        Cégünk többéves tapasztalattal áll az Ön rendelkezésére, hogy autója mindig biztonságosan és zökkenőmentesen közlekedhessen. A legmodernebb technológiával és szakértő csapattal végezzük el a gumiabroncsok cseréjét, javítását.
     </p>
-</div>
+</section>
+
+<!--<section class="galery-section-wrapper section">
+    <div class="photoes-wrapper">
+        <img src="{{ asset("images/galery_background.jpg") }}" alt="galéria">
+        <div class="galery-arrows">
+            <img src="{{ asset("images/index_section/arrow-left.svg") }}" alt="arrow">
+            <img src="{{ asset("images/index_section/arrow-right.svg") }}" alt="arrow">
+        </div>
+        <p class="galery-text">
+            Galériánkban betekintést nyerhet abba, hogyan gondoskodunk ügyfeleink autóiról. Legyen szó gumicseréről, javításról vagy karbantartásról, nálunk minden a precizitásról és a minőségről szól.
+        </p>
+    </div>
+</section>-->
 @endsection
