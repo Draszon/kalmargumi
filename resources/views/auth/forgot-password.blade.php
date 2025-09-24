a921c3c61a87fdab8e5f13e3b004ba807cce841a
Szentgyörgyi Péter <peti0202@windowslive.com>
2025-09-24 23:00:56 +0200

    bugfix és magyarosítás

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Elfelejtette jelszavát? Nem probléma. Csak adja meg nekünk az e-mail címét, és mi elküldünk Önnek egy jelszó-visszaállítási linket, amelynek segítségével új jelszót választhat.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Email Jelszó Visszaállít Link') }}
             </x-primary-button>
         </div>
     </form>
